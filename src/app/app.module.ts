96aa9c7c21b6eea8301ecfb683cc1b9bd4a5bc78
Stratos Samaras <samarasstratos@hotmail.com>
2021-09-27 20:25:37 +0300

    Main page structure - Header, footer, content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, MainPageComponent, HeaderComponent, FooterComponent],
     imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
